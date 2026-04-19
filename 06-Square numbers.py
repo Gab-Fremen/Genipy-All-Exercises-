b48c39a986229c93266f0bae6b28bb528cfbaaf5
Gabriel <fremen.gabs@gmail.com>
2026-04-19 13:54:46 -0300

    commit -m "Add the exercise 'Square numbers'"

---
+++ 06-Square numbers.py
@@ -0,0 +1,10 @@
+"""
+Print the 10 first square number, one per line.
+
+Start at 0, so the first square number is 02, followed by 12, 22, and so on up to 92.
+"""
+
+#define a for loop that goes from 0 to 9 and prints the square of each number in the loop to the console.
+for i in range(10):
+    print(i ** 2)
+
