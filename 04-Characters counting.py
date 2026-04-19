d60eb92ffa963de8212186383cc9df24fb904e68
Gabriel <fremen.gabs@gmail.com>
2026-04-19 01:50:16 -0300

    commit -m "Add the exercise 'Characters counting' to the repository"

---
+++ 04-Characters counting.py
@@ -0,0 +1,24 @@
+"""
+Print the number of characters in the following paragraph:
+
+Python is an easy to learn, powerful
+programming language. It has efficient high-level data structures and
+a simple but effective approach to object-oriented
+programming. Python’s elegant syntax and dynamic typing, together with
+its interpreted nature, make it an ideal language for scripting and
+rapid application development in many areas on most platforms.
+"""
+
+# I prefilled this variable for you, you don't need to touch it.
+
+whetting_your_appetite = """Python is an easy to learn, powerful
+programming language. It has efficient high-level data structures and
+a simple but effective approach to object-oriented
+programming. Python’s elegant syntax and dynamic typing, together with
+its interpreted nature, make it an ideal language for scripting and
+rapid application development in many areas on most platforms."""
+
+# Enter your code below:
+
+#A code that prints the number of characters in the paragraph contained by the variable "whetting_your_appetite" to the console.
+print(len(whetting_your_appetite))
