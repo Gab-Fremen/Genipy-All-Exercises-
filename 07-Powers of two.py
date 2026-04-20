47a3f51bb64a4872c4f8a27552360866282eab32
Gabriel <fremen.gabs@gmail.com>
2026-04-20 02:20:08 -0300

    commit -m "Add the 'powers of two' exercise"

---
+++ 07-Powers of two.py
@@ -0,0 +1,11 @@
+"""
+Print the 10 first powers of two, one per line. (beware: not the first squares!)
+
+Start at 0, so the first power of two is 20 (which happen to be one), followed by 21, 22, and so on up to 29.
+
+
+"""
+
+#define a for loop that goes from 0 to 9 and prints the power of two of each number in the loop to the console.
+for i in range(10):
+    print(2 ** i)
