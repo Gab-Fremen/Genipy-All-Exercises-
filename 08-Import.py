f4ae870f7007a2ca89355ef76150cd943e2f0235
Gabriel <fremen.gabs@gmail.com>
2026-04-20 02:22:01 -0300

    commi -m "Add 'powers of two' exercise"

---
+++ 08-Import.py
@@ -0,0 +1,11 @@
+"""
+Write a script that print the factorial of 27.
+
+Use the factorial function from the math module.
+"""
+
+#import the math module
+import math
+
+#print the factorial of 27 to the console.
+print(math.factorial(27))
